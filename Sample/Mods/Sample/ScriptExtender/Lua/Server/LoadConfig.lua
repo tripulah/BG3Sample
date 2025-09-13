894a21443ca9c6f3b9ae7b0f7c3b25774f99a24c
tripulah <asquaredplusplus+tripulah@gmail.com>
2025-09-13 13:52:16 -0400

    Redid Config loading

--- Sample/Mods/Sample/ScriptExtender/Lua/Server/LoadConfig.lua
+++ Sample/Mods/Sample/ScriptExtender/Lua/Server/LoadConfig.lua
@@ -1,16 +1,46 @@
-local function LoadConfig()
-    -- hacky way of making the config appear in the AppSettings folder
-    local configFilePathPattern = string.gsub("Mods/%s/ScriptExtender/AbsoluteDefeatConfig.json", "'", "\'")
-    local modData = Ext.Mod.GetMod(ModuleUUID)
-    local filePath = configFilePathPattern:format(modData.Info.Directory)
-    local config = Ext.IO.LoadFile(filePath)
-    _P(filePath)
-    if config == nil then
-        config = Ext.IO.LoadFile(filePath, "data")
-    end
-    if config ~= nil then
-        Ext.IO.SaveFile(filePath, config)
-    end
+local authorConfigFilePathPattern = string.gsub("Mods/%s/ScriptExtender/AbsoluteDefeat.AuthorConfig.json", "'", "\'")
+local playerConfigFilePathPattern = string.gsub("Mods/%s/ScriptExtender/AbsoluteDefeat.PlayerConfig.json", "'", "\'")
+
+function RestorePlayerConfig()
+	local modData = Ext.Mod.GetMod(ModuleUUID)
+	local playerFilePath = playerConfigFilePathPattern:format(modData.Info.Directory)
+	local authorFilePath = authorConfigFilePathPattern:format(modData.Info.Directory)
+	local config = Ext.IO.LoadFile(playerFilePath)
+	if config == nil then
+		Ext.Utils.PrintWarning("Could not find config file at: " .. playerFilePath .. " creating one.")
+		local authorContent = Ext.IO.LoadFile(authorFilePath, "data")
+		if authorContent ~= nil then
+			local authorConfig = Ext.Json.Parse(authorContent)
+			if not authorConfig or not authorConfig.Scenarios then
+				Ext.Utils.PrintError("Invalid config structure in " .. authorFilePath)
+				return
+			end
+
+			local playerConfig = {
+				FileVersion = 1,
+				ModGuid = ModuleUUID,
+				Scenarios = {}
+			}
+
+			for _, scenario in pairs(authorConfig.Scenarios) do
+				playerConfig.Scenarios[scenario.Id] = { Weight = scenario.Weight }
+			end
+
+			-- encode to JSON
+			SaveJSONFile(playerFilePath, playerConfig)
+		else
+			Ext.Utils.PrintError("Could not find config file at: " .. authorFilePath)
+		end
+	end
 end
 
-LoadConfig()
+--- Saves the given content to a JSON file.
+--- @param filePath string The file path to save the content to.
+--- @param content table The table with content to save to the file.
+function SaveJSONFile(filePath, content)
+    local fileContent = Ext.Json.Stringify(content, { Beautify = true })
+    Ext.IO.SaveFile(filePath, fileContent)
+    _P("[SaveJSONFile] File saved to " .. filePath)
+end
+
+RestorePlayerConfig()
